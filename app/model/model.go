841e22fa85bd9a48b7509af734702110e7f21786
mathieul <mathieul@gmail.com>
2012-11-24 20:35:27 -0800

    WIP: start implementing AttributeOwner custom type

--- app/model/model.go
+++ app/model/model.go
@@ -1,4 +1,33 @@
 package model
 
+type Fields []string
 type Attributes map[string]interface{}
 
+type AttributeOwner struct {
+	hasField map[string]bool
+	attributes Attributes
+}
+
+func normalizeAttributes(hasField map[string]bool, attributes *Attributes) *Attributes {
+	normalized := make(Attributes, len(*attributes))
+	for key, value := range *attributes {
+		if hasField[key] {
+			normalized[key] = value
+		}
+	}
+	return &normalized
+}
+
+func NewAttributeOwner(fields *Fields, attributes *Attributes) *AttributeOwner {
+	hasField := make(map[string]bool, len(*fields))
+	for _, name := range *fields {
+		hasField[name] = true
+	}
+	attributes = normalizeAttributes(hasField, attributes)
+	return &AttributeOwner{hasField, *attributes}
+}
+
+func (owner *AttributeOwner) Get(key string) (value interface{}, found bool) {
+	value, found = owner.attributes[key]
+	return value, found
+}
--- app/model/model_test.go
+++ app/model/model_test.go
@@ -9,12 +9,13 @@ import (
 func Test(t *testing.T) { TestingT(t) }
 
 type Person struct {
-	model.Model
+	model.AttributeOwner
 }
 
 func NewPerson(attributes model.Attributes) (*Person) {
-	fields := []string{"firstName", "lastName"}
-	return &Person{model.NewModel(fields, attributes)}
+	fields := model.Fields{"firstName", "lastName"}
+	owner := model.NewAttributeOwner(&fields, &attributes)
+	return &Person{*owner}
 }
 
 type ModelSuite struct{
@@ -24,13 +25,18 @@ type ModelSuite struct{
 var _ = Suite(&ModelSuite{})
 
 func (s *ModelSuite) SetUpTest(c *C) {
-	s.person = NewPerson(model.Attributes{"firstName", "John", "lastName", "Doe"})
+	s.person = NewPerson(model.Attributes{"firstName": "John", "lastName": "Doe"})
 }
 
 func (s *ModelSuite) TearDownTest(c *C) {
 }
 
 func (s *ModelSuite) TestItReadsAttributesWithGet(c *C) {
-    c.Assert("John", Equals, s.person.Get("firstName"))
-    c.Assert("Doe", Equals, s.person.Get("lastName"))
+	firstName, found := s.person.Get("firstName")
+	c.Assert(true, Equals, found)
+    c.Assert("John", Equals, firstName)
+
+    firstName, found = s.person.Get("unknown")
+	c.Assert(false, Equals, found)
+    c.Assert(firstName, IsNil)
 }
