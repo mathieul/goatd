591ed1ada11131e48bb5c49de63b4f2548c785d9
mathieul <mathieul@gmail.com>
2012-11-26 21:20:06 -0800

    dynamically set team attributes and generate Uid

--- app/models/team_test.go
+++ app/models/team_test.go
@@ -20,7 +20,7 @@ func (s *TeamSuite) TearDownTest(c *C) {
 }
 
 func (s *TeamSuite) TestCreateTeamWithName(c *C) {
-    team := models.CreateTeam(models.Attrs{"Name", "Metallica"})
-    c.Assert("Metallica", Equals, team.Name())
-    c.Assert(team.Uid(), NotNil)
+    team := models.CreateTeam(models.Attrs{"Name": "Metallica"})
+    c.Assert(team.Name(), Equals, "Metallica")
+    c.Assert(len(team.Uid()), Equals, 8 + 1 + 8)
 }
