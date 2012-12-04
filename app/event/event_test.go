e0e6c4dae248b86ed6d0bf523bf98f3f85269dfd
Mathieu Lajugie <mathieu@caring.com>
2012-12-04 09:18:58 -0800

    update event specs for SubscribeTo and SubscribeToEvent

--- app/event/event_test.go
+++ app/event/event_test.go
@@ -28,12 +28,36 @@ func (s *EventSuite) TearDownTest(c *C) {
 func (s *EventSuite) TestPublishEvent(c *C) {
     var received event.Event
     go func() {
-        outgoing := event.Manager().SubscribeToAllEvents()
-        received = <- outgoing
+        incoming := event.Manager().SubscribeToAll()
+        received = <- incoming
     }()
     event.Manager().PublishEvent(event.OfferTask, s.identity, []string{"string"})
     time.Sleep(200 * time.Millisecond)
     c.Assert(received.Kind, Equals, event.OfferTask)
     c.Assert(received.Identity, Equals, s.identity)
     c.Assert(received.Data[0], Equals, "string")
+}
+
+func (s *EventSuite) TestSubscribingToSomeEvents(c *C) {
+    received1 := make([]event.Event, 0, 2)
+    received2 := make([]event.Event, 0, 2)
+    go func() {
+        incoming := event.Manager().SubscribeTo([]event.Event{event.OfferTask, event.CompleteTask})
+        received1[0] = <- incoming
+        received1[1] = <- incoming
+    }()
+    go func() {
+        incoming := event.Manager().SubscribeToEvent(event.CompleteTask)
+        received2[0] = <- incoming
+        received2[1] = <- incoming
+    }()
+    event.Manager().PublishEvent(event.CompleteTask, s.identity, []string{"complete 1"})
+    event.Manager().PublishEvent(event.OfferTask, s.identity, []string{"offer"})
+    event.Manager().PublishEvent(event.CompleteTask, s.identity, []string{"complete 2"})
+
+    time.Sleep(200 * time.Millisecond)
+    c.Assert(received1[0].Data[0], Equals, "complete 1")
+    c.Assert(received1[1].Data[0], Equals, "offer")
+    c.Assert(received1[0].Data[0], Equals, "complete 1")
+    c.Assert(received1[1].Data[0], Equals, "complete 2")
 }
