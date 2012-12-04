d3b4450c28c208d02acecf800c6e1a77371daf35
mathieul <mathieul@gmail.com>
2012-12-04 06:24:25 -0800

    convert teammate collection

--- app/models/teammate.go
+++ app/models/teammate.go
@@ -19,9 +19,8 @@ func NewTeammate(attributes Attrs) *Teammate {
     return newModel(&Teammate{}, &attributes).(*Teammate)
 }
 
-func CreateTeammate(attributes Attrs, team *Team) *Teammate {
+func CreateTeammate(attributes Attrs) *Teammate {
     teammate := NewTeammate(attributes)
-    teammate.SetTeam(team)
     teammate.Save()
     return teammate
 }
@@ -47,27 +46,69 @@ func (teammate Teammate) Team() (team *Team) {
  * Teammates
  */
 
+// type Teammates struct {
+//     owner identification.Identity
+//     items []*Teammate
+// }
+
+// func NewTeammates(owner identification.Identity) (teammates *Teammates) {
+//     teammates = new(Teammates)
+//     teammates.owner = owner
+//     return teammates
+// }
+
+// func (teammates *Teammates) Create(attributes Attrs) (teammate *Teammate) {
+//     attributes = teammates.owner.AddToAttributes(attributes)
+//     team := teammates.owner.Value().(*Team)
+//     teammate = CreateTeammate(attributes, team)
+//     teammates.items = append(teammates.items, teammate)
+//     return teammate
+// }
+
+// func (teammates *Teammates) Slice() (slice []*Teammate) {
+//     slice = make([]*Teammate, len(teammates.items))
+//     copy(slice, teammates.items)
+//     return slice
+// }
+
 type Teammates struct {
-    owner identification.Identity
-    items []*Teammate
+    Collection
+}
+
+func toTeammateSlice(source []interface{}) []*Teammate {
+    teammates := make([]*Teammate, 0, len(source))
+    for _, teammate := range source {
+        teammates = append(teammates, teammate.(*Teammate))
+    }
+    return teammates
 }
 
 func NewTeammates(owner identification.Identity) (teammates *Teammates) {
     teammates = new(Teammates)
-    teammates.owner = owner
+    teammates.Collection = NewCollection(func(attributes Attrs, lonerTeam interface{}) interface{} {
+        teammate := CreateTeammate(attributes)
+        teammate.SetTeam(lonerTeam.(*Team))
+        return teammate
+    }, owner)
     return teammates
 }
 
 func (teammates *Teammates) Create(attributes Attrs) (teammate *Teammate) {
-    attributes = teammates.owner.AddToAttributes(attributes)
-    team := teammates.owner.Value().(*Team)
-    teammate = CreateTeammate(attributes, team)
-    teammates.items = append(teammates.items, teammate)
-    return teammate
+    return teammates.Collection.Create(attributes).(*Teammate)
 }
 
-func (teammates *Teammates) Slice() (slice []*Teammate) {
-    slice = make([]*Teammate, len(teammates.items))
-    copy(slice, teammates.items)
-    return slice
+func (teammates Teammates) Slice() []*Teammate {
+    return toTeammateSlice(teammates.Collection.Slice())
+}
+
+func (teammates Teammates) Find(uid string) *Teammate {
+    return teammates.Collection.Find(uid).(*Teammate)
+}
+
+func (teammates Teammates) FindAll(uids []string) []*Teammate {
+    return toTeammateSlice(teammates.Collection.FindAll(uids))
+}
+
+func (teammates Teammates) Select(tester func(interface{}) bool) (result []*Teammate) {
+    return toTeammateSlice(teammates.Collection.Select(tester))
 }
--- app/models/teammate_test.go
+++ app/models/teammate_test.go
@@ -3,6 +3,7 @@ package models_test
 import (
     . "launchpad.net/gocheck"
     "testing"
+    "strings"
     "goatd/app/models"
 )
 
@@ -37,4 +38,31 @@ func (s *TeammateSuite) TestReturnSlice(c *C) {
     t2 := s.teammates.Create(models.Attrs{"Name": "Egret"})
     t3 := s.teammates.Create(models.Attrs{"Name": "Aria"})
     c.Assert(s.teammates.Slice(), DeepEquals, []*models.Teammate{t1, t2,t3})
+}
+
+func (s *TeammateSuite) TestFindTeammate(c *C) {
+    s.teammates.Create(models.Attrs{"Name": "Jon"})
+    egret := s.teammates.Create(models.Attrs{"Name": "Egret"})
+    c.Assert(s.teammates.Find(egret.Uid()), DeepEquals, egret)
+}
+
+func (s *TeammateSuite) TestFindTeammateSlice(c *C) {
+    t1 := s.teammates.Create(models.Attrs{"Name": "One"})
+    s.teammates.Create(models.Attrs{"Name": "Two"})
+    t3 := s.teammates.Create(models.Attrs{"Name": "Three"})
+    c.Assert(s.teammates.FindAll([]string{t1.Uid(), t3.Uid()}),
+             DeepEquals,
+             []*models.Teammate{t1, t3})
+}
+
+func (s *TeammateSuite) TestSelectTeammates(c *C) {
+    tyrion := s.teammates.Create(models.Attrs{"Name": "Tyrion Lanister"})
+    s.teammates.Create(models.Attrs{"Name": "Jon Snow"})
+    jamie := s.teammates.Create(models.Attrs{"Name": "Jamie Lanister"})
+    c.Assert(s.teammates.Select(func (item interface{}) bool {
+            teammate := item.(*models.Teammate)
+            return strings.Contains(teammate.Name(), "Lanister")
+        }),
+        DeepEquals,
+        []*models.Teammate{tyrion, jamie})
 }
