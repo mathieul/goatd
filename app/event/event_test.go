3d1d69df0977aa4d41bc1968e4b7dd0c0d0100d0
Mathieu Lajugie <mathieu@caring.com>
2012-12-08 21:22:31 -0800

    merge identification module into event and refactor

--- app/event/event_test.go
+++ app/event/event_test.go
@@ -3,59 +3,60 @@ package event_test
 import (
     . "launchpad.net/gocheck"
     "testing"
-    "goatd/app.old/identification"
-    "goatd/app.old/event"
+    "goatd/app/event"
     "time"
 )
 
 func Test(t *testing.T) { TestingT(t) }
 
 type EventSuite struct{
-    identity identification.Identity
+    busManager *event.BusManager
+    identity event.Identity
 }
 
 var _ = Suite(&EventSuite{})
 
 func (s *EventSuite) SetUpTest(c *C) {
-    event.Manager().Start()
+    s.busManager = event.NewBusManager()
+    s.busManager.Start()
     s.identity.Set("Team", "qazwsx098", nil)
 }
 
 func (s *EventSuite) TearDownTest(c *C) {
-    event.Manager().Stop()
+    s.busManager.Stop()
 }
 
 func (s *EventSuite) TestPublishEvent(c *C) {
     var received event.Event
     go func() {
-        incoming := event.Manager().SubscribeToAll()
+        incoming := s.busManager.SubscribeToAll()
         received = <- incoming
     }()
-    event.Manager().PublishEvent(event.KindOfferTask, s.identity, []string{"string"})
+    s.busManager.PublishEvent(event.OfferTask, s.identity, []interface{}{"string"})
     time.Sleep(200 * time.Millisecond)
-    c.Assert(received.Kind, Equals, event.KindOfferTask)
+    c.Assert(received.Kind, Equals, event.OfferTask)
     c.Assert(received.Identity, Equals, s.identity)
-    c.Assert(received.Data[0], Equals, "string")
+    c.Assert(received.Data[0].(string), Equals, "string")
 }
 
 func (s *EventSuite) TestSubscribingToSomeEvents(c *C) {
     var e11, e12, e21, e22 event.Event
     go func() {
-        incoming := event.Manager().SubscribeTo([]event.Kind{event.KindOfferTask, event.KindCompleteTask})
+        incoming := s.busManager.SubscribeTo([]event.Kind{event.OfferTask, event.CompleteTask})
         e11 = <- incoming
         e12 = <- incoming
     }()
     go func() {
-        incoming := event.Manager().SubscribeToEvent(event.KindCompleteTask)
+        incoming := s.busManager.SubscribeToEvent(event.CompleteTask)
         e21 = <- incoming
         e22 = <- incoming
     }()
-    event.Manager().PublishEvent(event.KindCompleteTask, s.identity, []string{"complete 1"})
-    event.Manager().PublishEvent(event.KindOfferTask, s.identity, []string{"offer"})
+    s.busManager.PublishEvent(event.CompleteTask, s.identity, []interface{}{"complete 1"})
+    s.busManager.PublishEvent(event.OfferTask, s.identity, []interface{}{"offer"})
 
     time.Sleep(200 * time.Millisecond)
-    c.Assert(e11.Data[0], Equals, "complete 1")
-    c.Assert(e12.Data[0], Equals, "offer")
-    c.Assert(e21.Data[0], Equals, "complete 1")
+    c.Assert(e11.Data[0].(string), Equals, "complete 1")
+    c.Assert(e12.Data[0].(string), Equals, "offer")
+    c.Assert(e21.Data[0].(string), Equals, "complete 1")
     c.Assert(e22.Data, IsNil)
 }
