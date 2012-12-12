efbf9dc049b6adf76281e0d0860d72f5f968dde5
mathieul <mathieul@gmail.com>
2012-12-11 19:43:15 -0800

    finish draft of new acceptance test

--- acceptance/queue_assign_complete_task.go
+++ acceptance/queue_assign_complete_task.go
@@ -22,6 +22,7 @@ var _ = Suite(&AcceptanceSuite{})
 func (s *AcceptanceSuite) SetUpTest(c *C) {
     s.busManager = event.NewBusManager()
     s.busManager.Start()
+    s.store = store.NewMemoryStore(s.busManager)
     s.store.Start()
 }
 
@@ -36,34 +37,28 @@ func (s *AcceptanceSuite) TestAssignsATaskToATeamMate(c *C) {
 
     # provisioning
     team := s.store.Teams.Create(model.A{"Name": "Jones Household"})
-    distributor := dispatch.NewDistributor(team)
+    distributor := dispatch.NewDistributor(team, s.busManager, s.store)
     mate := team.Teammates.Create(model.A{"Name": "Jack"})
     queue := team.Queues.Create(model.A{"Name": "Duties"})
+    skill := team.Skills.Create(model.A{"QueueUid": queue.Uid(), "TeammateUid": mate.Uid()})
 
     # keep track of events received
     var eventOne, eventTwo, eventThree event.Event
     go func() {
         incoming := event.Manager().SubscribeTo([]event.Kind{
-            event.KindOfferTask, event.KindAcceptTask, event.KindCompleteTask,
+            event.EventOfferTask, event.EventAcceptTask, event.EventCompleteTask,
         })
         eventOne = <-incoming; fmt.Println("eventOne:", eventOne)
         eventTwo = <-incoming; fmt.Println("eventTwo:", eventTwo)
         eventThree = <-incoming; fmt.Println("eventThree:", eventThree)
     }()
 
-    distributor.AddTeammateToQueue(queue, mate, model.LevelHigh)
-    skills := team.Skills.Select(func (item interface{}) bool {
-        skill := item.(*model.Skill)
-        return skill.TeammateUid() == mate.Uid() && skill.QueueUid() == queue.Uid()
-    })
-    c.Assert(len(skills), Equals, 1)
-    c.Assert(skills[0].Level(), Equals, model.LevelHigh)
-
+    # create task and get have the teammate process it
     c.Assert(mate.Status(), Equals, model.StatusSignedOut)
     mate.SignIn()
     c.Assert(mate.Status(), Equals, model.StatusOnBreak)
 
-    task := team.Tasks.Create(model.Attrs{"Title": "thank Jones family"})
+    task := team.Tasks.Create(model.A{"Title": "take out the trash"})
     c.Assert(task.Status(), Equals, model.StatusCreated)
     task.Enqueue(queue)
     c.Assert(task.Status(), Equals, model.StatusQueued)
@@ -74,7 +69,7 @@ func (s *AcceptanceSuite) TestAssignsATaskToATeamMate(c *C) {
     c.Assert(mate.CurrentTask(), DeepEquals, task)
     c.Assert(task.Status(), Equals, model.StatusOffered)
 
-    c.Assert(eventOne.Kind, Equals, event.KindOfferTask)
+    c.Assert(eventOne.Kind, Equals, event.EventOfferTask)
     c.Assert(eventOne.Data[0], Equals, mate.Uid())
     c.Assert(eventOne.Data[1], Equals, task.Uid())
 
@@ -85,7 +80,7 @@ func (s *AcceptanceSuite) TestAssignsATaskToATeamMate(c *C) {
     c.Assert(task.Status(), Equals, model.StatusAssigned)
     c.Assert(queue.QueuedTasks(), DeepEquals, []*model.Task{task})
 
-    c.Assert(eventTwo.Kind, Equals, event.KindAcceptTask)
+    c.Assert(eventTwo.Kind, Equals, event.EventAcceptTask)
     c.Assert(eventTwo.Data[0], Equals, mate.Uid())
     c.Assert(eventTwo.Data[1], Equals, task.Uid())
 
@@ -96,7 +91,7 @@ func (s *AcceptanceSuite) TestAssignsATaskToATeamMate(c *C) {
     c.Assert(task.Status(), Equals, model.StatusCompleted)
     c.Assert(queue.QueuedTasks(), DeepEquals, []*model.Task{})
 
-    c.Assert(eventThree.Kind, Equals, event.KindCompleteTask)
+    c.Assert(eventThree.Kind, Equals, event.EventCompleteTask)
     c.Assert(eventThree.Data[0], Equals, mate.Uid())
     c.Assert(eventThree.Data[1], Equals, task.Uid())
 
