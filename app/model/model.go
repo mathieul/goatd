e0391c56fa2ac37d9c59473c98426e2907c5558b
mathieul <mathieul@gmail.com>
2012-12-15 19:46:51 -0800

    extract model collection into its own file

--- app/model/model.go
+++ app/model/model.go
@@ -4,7 +4,6 @@ import (
     "reflect"
     "log"
     "fmt"
-    "goatd/app/event"
 )
 
 const (
@@ -68,67 +67,3 @@ func simpleMethodCall(model Model, methodName string) interface{} {
     return result[0].Interface()
 }
 
-/*
- * Collection
- */
-
-type Model interface {
-    Uid() string
-    Copy() Model
-}
-type CollectionCreator func (A) Model
-type Collection struct {
-    creator CollectionCreator
-    Items []Model
-    owner *event.Identity
-}
-
-func NewCollection(creator CollectionCreator, owner *event.Identity) (collection *Collection) {
-    collection = new(Collection)
-    collection.creator = creator
-    collection.owner = owner
-    return collection
-}
-
-func (collection *Collection) New(attributes A) interface{} {
-    if collection.owner != nil {
-        attributes = collection.owner.AddToAttributes(attributes)
-    }
-    model := collection.creator(attributes)
-    collection.Items = append(collection.Items, model)
-    return model
-}
-
-func (collection *Collection) Slice() []Model {
-    return collection.Items
-}
-
-func (collection Collection) Find(uid string) Model {
-    found := collection.FindAll([]string{uid})
-    if len(found) == 0 {
-        return nil
-    }
-    return found[0]
-}
-
-func (collection Collection) FindAll(uids []string) (found []Model) {
-    for _, candidate := range collection.Items {
-        candidateUid := simpleMethodCall(candidate, "Uid").(string)
-        for _, uid := range uids {
-            if candidateUid == uid {
-                found = append(found, candidate)
-            }
-        }
-    }
-    return found
-}
-
-func (collection Collection) Select(tester func(interface{}) bool) (result []Model) {
-    result = make([]Model, 0)
-    for _, item := range collection.Items {
-        if tester(item) {
-            result = append(result, item)
-        }
-    }
-    return result
-}
