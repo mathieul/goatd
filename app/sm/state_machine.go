2303486cc0629e716c4c96f7b694c3f8d38bebc7
Mathieu Lajugie <mathieu@caring.com>
2013-01-24 11:00:56 -0800

    make multi transition events work

--- app/sm/state_machine.go
+++ app/sm/state_machine.go
@@ -8,7 +8,7 @@ import (
 /*
  * Constants and initialized vars
  */
-var NoAction Action
+var NoAction func ([]interface{}) bool
 
 func init() {
     NoAction = func (args []interface{}) bool { return true }
@@ -19,7 +19,6 @@ func init() {
  */
 type Event uint
 type Status uint
-type Action func ([]interface{}) bool
 type statusTargetMap map[Status]statusTarget
 type transitionMap map[Event]statusTargetMap
 
@@ -28,50 +27,50 @@ type transitionMap map[Event]statusTargetMap
  */
 type statusTarget struct {
     value  Status
-    action Action
+    action func ([]interface{}) bool
 }
 
 type Builder struct {
     stateMachine *StateMachine
+    event Event
 }
 
 func newBuilder(stateMachine *StateMachine) *Builder {
-    return &Builder{stateMachine}
+    return &Builder{stateMachine, 0}
 }
 
-func (builder Builder) EventSingleTransition(event Event, from, to Status, action Action) {
+func (builder Builder) EventSingleTransition(event Event, from, to Status, action func ([]interface{}) bool) {
+    newBuilder := Builder{builder.stateMachine, event}
+    newBuilder.Transition(from, to, action)
+}
+
+func (builder Builder) EventMultiTransitions(event Event, callback func(Builder)) {
+    newBuilder := Builder{builder.stateMachine, event}
+    callback(newBuilder)
+}
+
+func (builder Builder) Transition(from, to Status, action func ([]interface{}) bool) {
     target := statusTarget{to, action}
-    if transitions, found := builder.stateMachine.eventTransitions[event]; found {
-        fmt.Println("EventSingleTransition(", event, "): found => set ", from)
+    if transitions, found := builder.stateMachine.eventTransitions[builder.event]; found {
+        fmt.Println("EventSingleTransition(", builder.event, "): found => set ", from)
         transitions[from] = target
     } else {
-        fmt.Println("EventSingleTransition(", event, "): not found - allocate")
+        fmt.Println("EventSingleTransition(", builder.event, "): not found - allocate")
         transitions = make(statusTargetMap, 1)
         transitions[from] = target
-        builder.stateMachine.eventTransitions[event] = transitions
+        builder.stateMachine.eventTransitions[builder.event] = transitions
     }
-}
-
-func (builder Builder) EventMultiTransitions(event Event, callback func(Transitioner)) {
 }
 
 func (builder Builder) Event(event Event, args ...interface{}) {
     switch len(args) {
     case 1:
-        builder.EventMultiTransitions(event, args[0].(func(Transitioner)))
+        builder.EventMultiTransitions(event, args[0].(func(Builder)))
     case 3:
-        builder.EventSingleTransition(event, args[0].(Status), args[1].(Status), args[2].(Action))
+        builder.EventSingleTransition(event, args[0].(Status), args[1].(Status), args[2].(func ([]interface{}) bool))
     default:
         log.Fatal(fmt.Errorf("sm.Event(): invalid call with %d arguments", len(args) + 1))
     }
-}
-
-/*
- * Transitioner
- */
-type Transitioner struct {}
-
-func (transitioner Transitioner) Transition(from, to Status, callback Action) {
 }
 
 /*
@@ -86,12 +85,12 @@ func (stateMachine StateMachine) Status() Status {
     return stateMachine.status
 }
 
-func (stateMachine *StateMachine) Trigger(event Event) bool {
+func (stateMachine *StateMachine) Trigger(event Event, args ...interface{}) bool {
     if transitions, found := stateMachine.eventTransitions[event]; found {
         if target, listed := transitions[stateMachine.status]; listed {
             stateMachine.status = target.value
             if target.action != nil {
-                target.action([]interface{}{})
+                target.action(args)
             }
             return true
         }
@@ -102,6 +101,7 @@ func (stateMachine *StateMachine) Trigger(event Event) bool {
 func NewStateMachine(status Status, callback func (Builder)) (stateMachine *StateMachine) {
     eventTransitions := make(transitionMap, 5)
     stateMachine = &StateMachine{status, eventTransitions}
-    callback(*newBuilder(stateMachine))
+    builder := *newBuilder(stateMachine)
+    callback(builder)
     return stateMachine
 }
--- app/sm/state_machine_test.go
+++ app/sm/state_machine_test.go
@@ -43,33 +43,50 @@ func (s *StateMachineSuite) TestSingleTransitionEventsNoAction(c *C) {
     })
     c.Assert(sm.Trigger(eventOpen), Equals, true)
     c.Assert(sm.Status(), Equals, statusOpened)
+    c.Assert(sm.Trigger(eventClose), Equals, true)
+    c.Assert(sm.Status(), Equals, statusClosed)
 }
 
-// func (s *StateMachineSuite) TestUniTransitionEventsWithAction(c *C) {
-//     state := "not set"
-//     sm := sm.NewStateMachine(statusClosed, func (b sm.Builder) {
-//         b.Event(eventOpen, statusClosed, statusOpened, func (args []interface{}) bool {
-//             state = "opened"
-//             return true
-//         })
-//         b.Event(eventClose, statusOpened, statusClosed, func (args []interface{}) bool {
-//             state = "closed"
-//             return true
-//         })
-//     })
-//     c.Assert(sm.Trigger(eventOpen), Equals, true)
-//     c.Assert(state, Equals, "opened")
-//     c.Assert(sm.Trigger(eventClose), Equals, true)
-//     c.Assert(state, Equals, "closed")
-// }
+func (s *StateMachineSuite) TestUniTransitionEventsWithAction(c *C) {
+    state := "not set"
+    sm := sm.NewStateMachine(statusClosed, func (b sm.Builder) {
+        b.Event(eventOpen, statusClosed, statusOpened, func (args []interface{}) bool {
+            state = "opened"
+            return true
+        })
+        b.Event(eventClose, statusOpened, statusClosed, func (args []interface{}) bool {
+            state = "closed"
+            return true
+        })
+    })
+    c.Assert(sm.Trigger(eventOpen), Equals, true)
+    c.Assert(state, Equals, "opened")
+    c.Assert(sm.Trigger(eventClose), Equals, true)
+    c.Assert(state, Equals, "closed")
+}
+
+func (s *StateMachineSuite) TestTriggeringWithArguments(c *C) {
+    var hello string
+    var fortyTwo int
+    sm := sm.NewStateMachine(statusClosed, func (b sm.Builder) {
+        b.Event(eventOpen, statusClosed, statusOpened, func (args []interface{}) bool {
+            hello = args[0].(string)
+            fortyTwo = args[1].(int)
+            return true
+        })
+    })
+    c.Assert(sm.Trigger(eventOpen, "hello", 42), Equals, true)
+    c.Assert(hello, Equals, "hello")
+    c.Assert(fortyTwo, Equals, 42)
+}
 
 // func (s *StateMachineSuite) TestMultiTransitionEvents(c *C) {
 //     sm := sm.NewStateMachine(statusClosed, func (b sm.Builder) {
 //         b.Event(eventOpen, statusClosed, statusOpened, sm.NoAction)
 //         b.Event(eventClose, statusOpened, statusClosed, sm.NoAction)
-//         b.Event(eventLock, func (t sm.Transitioner) {
-//             t.Transition(statusClosed, statusLocked, sm.NoAction)
-//             t.Transition(statusOpened, statusLockedOpen, sm.NoAction)
+//         b.Event(eventLock, func (b sm.Builder) {
+//             b.Transition(statusClosed, statusLocked, sm.NoAction)
+//             b.Transition(statusOpened, statusLockedOpen, sm.NoAction)
 //         })
 //     })
 //     c.Assert(sm.Trigger(eventLock), Equals, true)
