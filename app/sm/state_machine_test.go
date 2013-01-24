c0daafc4fa8479a09a17165aa1c1a7f18db620d8
Mathieu Lajugie <mathieu@caring.com>
2013-01-23 17:38:22 -0800

    start specing out the state machine module

--- app/sm/state_machine_test.go
+++ app/sm/state_machine_test.go
@@ -11,13 +11,19 @@ func Test(t *testing.T) { TestingT(t) }
 /*
  * Setup
  */
-var eventOpen, eventClose sm.Event
-var statusOpened, statusClosed sm.Status
 
-func init() {
-    eventOpen, eventClose = 1, 2
-    statusOpened, statusClosed = 1, 2
-}
+const (
+    eventOpen sm.Event = iota + 1
+    eventClose
+    eventLock
+)
+
+const (
+    statusOpened sm.Status = iota + 1
+    statusClosed
+    statusLocked
+    statusLockedOpen
+)
 
 type StateMachineSuite struct {
     sm *sm.StateMachine
@@ -25,14 +31,47 @@ type StateMachineSuite struct {
 
 var _ = Suite(&StateMachineSuite{})
 
-func (s *StateMachineSuite) TestCreateStateMachine(c *C) {
-    sm := sm.NewStateMachine(statusClosed, func (builder sm.Builder) {
-        builder.Event(eventOpen, statusClosed, statusOpened, sm.NoAction)
-        builder.Event(eventClose, statusOpened, statusClosed, func (args []interface{}) bool {
+func (s *StateMachineSuite) TestCreateStateMachineSetsInitialStatus(c *C) {
+    sm := sm.NewStateMachine(statusClosed, func (b sm.Builder) {})
+    c.Assert(sm.Status(), Equals, statusClosed)
+}
+
+func (s *StateMachineSuite) TestUniTransitionEventsNoAction(c *C) {
+    sm := sm.NewStateMachine(statusClosed, func (b sm.Builder) {
+        b.Event(eventOpen, statusClosed, statusOpened, sm.NoAction)
+        b.Event(eventClose, statusOpened, statusClosed, sm.NoAction)
+    })
+    c.Assert(sm.Trigger(eventOpen), Equals, true)
+    c.Assert(sm.Status(), Equals, statusOpened)
+}
+
+func (s *StateMachineSuite) TestUniTransitionEventsWithAction(c *C) {
+    state := "not set"
+    sm := sm.NewStateMachine(statusClosed, func (b sm.Builder) {
+        b.Event(eventOpen, statusClosed, statusOpened, func (args []interface{}) bool {
+            state = "opened"
+            return true
+        })
+        b.Event(eventClose, statusOpened, statusClosed, func (args []interface{}) bool {
+            state = "closed"
             return true
         })
     })
-    c.Assert(sm.Status(), Equals, statusClosed)
     c.Assert(sm.Trigger(eventOpen), Equals, true)
-    c.Assert(sm.Status(), Equals, statusOpened)
+    c.Assert(state, Equals, "opened")
+    c.Assert(sm.Trigger(eventClose), Equals, true)
+    c.Assert(state, Equals, "closed")
+}
+
+func (s *StateMachineSuite) TestMultiTransitionEvents(c *C) {
+    sm := sm.NewStateMachine(statusDraft, func (b sm.Builder) {
+        b.Event(eventOpen, statusClosed, statusOpened, sm.NoAction)
+        b.Event(eventClose, statusOpened, statusClosed, sm.NoAction)
+        b.Event(eventLock, func (t sm.Transitioner) {
+            t.Transition(statusClosed, statusLocked, sm.NoAction)
+            t.Transition(statusOpened, statusLockedOpen, sm.NoAction)
+        })
+    })
+    c.Assert(sm.Trigger(eventLock), Equals, true)
+    c.Assert(sm.Status(), Equals, statusLockedOpened)
 }
