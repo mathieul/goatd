6dc0b546b005be7bfc2f6a8bf40d5773957989fb
Mathieu Lajugie <mathieul@gmail.com>
2013-02-07 22:33:07 -0800

    testing zmq client

--- webatd/app/controllers/teams_controller.rb
+++ webatd/app/controllers/teams_controller.rb
@@ -6,7 +6,28 @@ class TeamsController < ApplicationController
   respond_to :json
 
   def index
-    list = []
+    list = [{name: send_message("Blah")}]
     respond_with list
+  end
+
+  private
+
+  def send_message(message)
+    logger.debug ">>> #send_message(#{message.inspect}"
+    ctx = ZMQ::Context.create(1)
+    req = ctx.socket(ZMQ::REQ)
+    logger.debug ">>> before connect"
+    rc = req_sock.connect('tcp://127.0.0.1:5000')
+    raise "Failed to connect REQ socket" unless ZMQ::Util.resultcode_ok?(rc)
+    logger.debug ">>> before send_string"
+    rc = req_sock.send_string(message)
+    raise "Failed to send message" if error_check(rc)
+    rep = ''
+    logger.debug ">>> before recv_string"
+    rc = req_sock.recv_string(rep)
+    raise "Failed to receive response" if error_check(rc)
+    req_sock.close
+    ctx.terminate
+    rep
   end
 end
